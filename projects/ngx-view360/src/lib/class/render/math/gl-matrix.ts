ad3693184be2fc7bdfad6324b731b48855ad6df2
gegelascience <remi.verchere2@gmail.com>
2019-12-30 21:07:18 +0100

    delete unecessaries imports

--- projects/ngx-view360/src/lib/class/render/math/gl-matrix.ts
+++ projects/ngx-view360/src/lib/class/render/math/gl-matrix.ts
@@ -1,23 +1,12 @@
-import glMatrix from 'gl-matrix/cjs/common.js';
-import * as mat2 from 'gl-matrix/cjs/mat2';
-import * as mat2d from 'gl-matrix/cjs/mat2d';
+
 import * as mat3 from 'gl-matrix/cjs/mat3';
 import * as mat4 from 'gl-matrix/cjs/mat4';
 import * as quat from 'gl-matrix/cjs/quat';
-import * as quat2 from 'gl-matrix/cjs/quat2';
-import * as vec2 from 'gl-matrix/cjs/vec2';
 import * as vec3 from 'gl-matrix/cjs/vec3';
-import * as vec4 from 'gl-matrix/cjs/vec4';
 
 export {
-    glMatrix,
-    mat2,
-    mat2d,
     mat3,
     mat4,
     quat,
-    quat2,
-    vec2,
     vec3,
-    vec4,
 };
