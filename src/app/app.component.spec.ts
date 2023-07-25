d3910d1c131f3cc9d49d432da24e828609770afc
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-07-25 16:38:10 +0200

    upgrade unit test stack

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NgxView360Module } from 'ngx-view360';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,6 +8,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports:[
+        NgxView360Module
+      ]
     }).compileComponents();
   }));
 
@@ -22,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ngxPhotos360webxr');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngxPhotos360webxr app is running!');
-  });
 });
